e5067029e066d6a83f793a6958c423fcdbd21267
ChaoJung <chaojung@outlook.com>
2017-03-20 01:44:33 +0800

    add home activity for camera function

---
+++ app/src/main/java/com/irene/fintrip/HomeActivity.java
@@ -0,0 +1,151 @@
+package com.irene.fintrip;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
+import android.view.View;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+
+    String mCurrentPhotoPath;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final int  MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+
+    ItemAdapter itemAdapter;
+    RecyclerView rvRequest;
+    FloatingActionButton fabCreate;
+    List<Item> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(HomeActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(HomeActivity.this,
+                    Manifest.permission.CAMERA)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(HomeActivity.this,
+                        new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
+
+        setupView();
+        createItem();
+    }
+
+    private void setupView() {
+
+        rvRequest = (RecyclerView) findViewById(R.id.rvRequest);
+        // Initialize contacts
+        items = new ArrayList<>();
+//        items = Item.createItemList(1);
+        // Create adapter passing in the sample user data
+        itemAdapter = new ItemAdapter(this,items);
+        // Attach the adapter to the recyclerview to populate items
+        rvRequest.setAdapter(itemAdapter);
+        // Set layout manager to position the items
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvRequest.setLayoutManager(linearLayoutManager);
+        rvRequest.setHasFixedSize(true);
+
+    }
+
+    private void createItem() {
+        fabCreate = (FloatingActionButton) findViewById(R.id.fabCreate);
+        fabCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                }
+                else {
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+    private void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            Item item = new Item(true,BitMapToString(imageBitmap),"chao","10000");
+            items.add(0,item);
+            itemAdapter.notifyItemInserted(0);
+        }
+    }
+
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+}
---
+++ app/src/main/java/com/irene/fintrip/ItemAdapter.java
@@ -0,0 +1,126 @@
+package com.irene.fintrip;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ChaoJung on 2017/3/20.
+ */
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>  {
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public ImageView ivStar;
+        public ImageView ivProduct;
+        public TextView tvOwner;
+        public TextView tvPrice;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            ivStar = (ImageView) itemView.findViewById(R.id.ivStar);
+            ivProduct = (ImageView) itemView.findViewById(R.id.ivProduct);
+            tvOwner = (TextView) itemView.findViewById(R.id.tvOwner);
+            tvPrice = (TextView) itemView.findViewById(R.id.tvPrice);
+
+        }
+    }
+
+    // Store a member variable for the contacts
+    private List<Item> mItem;
+    // Store the context for easy access
+    private Context mContext;
+
+    // Clean all elements of the recycler
+    public void clear() {
+        mItem.clear();
+        notifyDataSetChanged();
+    }
+
+    // Pass in the contact array into the constructor
+    public ItemAdapter(Context context, List<Item> tweets) {
+        mItem = tweets;
+        mContext = context;
+    }
+
+    // Easy access to the context object in the recyclerview
+    private Context getContext() {
+        return mContext;
+    }
+
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_tobuy, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+
+        return viewHolder;
+    }
+
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Item item = mItem.get(position);
+
+//        viewHolder.getBinding().setVariable(com.codepath.apps.simpletweets.BR.tweet,tweet);
+//        viewHolder.getBinding().executePendingBindings();
+
+        // Set item views based on your views and data model
+        ImageView ivStar = viewHolder.ivStar;
+
+        ImageView ivProduct = viewHolder.ivProduct;
+        ivProduct.setImageBitmap(StringToBitMap(item.getImageUrl()));
+
+        TextView tvOwner = viewHolder.tvOwner;
+        tvOwner.setText(item.getOwner());
+
+        TextView tvPrice = viewHolder.tvPrice;
+        tvPrice.setText(item.getPrice());
+
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return mItem.size();
+    }
+
+    public Bitmap StringToBitMap(String encodedString){
+        try {
+            byte [] encodeByte= Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch(Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+}
