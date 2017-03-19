c59a23918da01388de4aa2f7e91113a744d17c44
Irene <irene3119@gmail.com>
2017-03-19 15:10:27 +0800

    Facebook Login Function

---
+++ app/src/main/java/com/irene/fintrip/BuylistActivity.java
@@ -0,0 +1,13 @@
+package com.irene.fintrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buylist);
+    }
+}
