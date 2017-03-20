42ea45603f0e96862bdf87e1ab1b272e0b63115d
ChaoJung <chaojung@outlook.com>
2017-03-21 03:02:06 +0800

    change bitmap to file path

--- app/src/main/java/com/irene/fintrip/ItemAdapter.java
+++ app/src/main/java/com/irene/fintrip/ItemAdapter.java
@@ -1,10 +1,8 @@
 package com.irene.fintrip;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -96,7 +94,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>  {
         ImageView ivStar = viewHolder.ivStar;
 
         ImageView ivProduct = viewHolder.ivProduct;
-        ivProduct.setImageBitmap(StringToBitMap(item.getImageUrl()));
+        ivProduct.setImageURI(Uri.parse(item.getImageUrl()));
 
         TextView tvOwner = viewHolder.tvOwner;
         tvOwner.setText(item.getOwner());
@@ -112,15 +110,5 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>  {
         return mItem.size();
     }
 
-    public Bitmap StringToBitMap(String encodedString){
-        try {
-            byte [] encodeByte= Base64.decode(encodedString,Base64.DEFAULT);
-            Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-            return bitmap;
-        } catch(Exception e) {
-            e.getMessage();
-            return null;
-        }
-    }
 
 }
