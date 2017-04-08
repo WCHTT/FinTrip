97abecf8556d78eed1987125d7a0c887deac688c
renee <yuwaj6@gmail.com>
2017-04-08 20:39:18 +0800

    Update camera icon position in details view

--- app/src/main/java/com/irene/fintrip/DetailsActivity.java
+++ app/src/main/java/com/irene/fintrip/DetailsActivity.java
@@ -152,6 +152,7 @@ public class DetailsActivity extends AppCompatActivity  implements EditItemFragm
                 currency);
         spinner.setAdapter(currencyList);
 
+        TextView buyerLabel = (TextView) findViewById(R.id.buyerLabel);
         tvLocation = (TextView) findViewById(location);
         owner = (TextView) findViewById(R.id.buyerName);
         tvTargetPrice = (TextView) findViewById(tPrice);
@@ -197,7 +198,10 @@ public class DetailsActivity extends AppCompatActivity  implements EditItemFragm
         if(item.getOwner()!=null && !item.getOwner().equals(""))
             owner.setText(item.getOwner());
         else
+        {
+            buyerLabel.setVisibility(View.GONE);
             owner.setVisibility(View.GONE);
+        }
 
         if(item.getPrice()!=0.0){
             itemPrice = item.getPrice();
